031c9a460743a431a75a2a5aade94add708a51d7
Antonio Cuni <anto.cuni@gmail.com>
2015-04-23 11:12:22 +0200

    move the Color class to its own file

--- pypytools/jitview.py
+++ pypytools/jitview.py
@@ -3,33 +3,12 @@ from cStringIO import StringIO
 import dis
 import bisect
 import linecache
-import pypyjit
+try:
+    import pypyjit
+except ImportError:
+    pass
 
-class Color:
-    black = '30'
-    darkred = '31'
-    darkgreen = '32'    
-    brown = '33'
-    darkblue = '34'
-    purple = '35'
-    teal = '36'
-    lightgray = '37'
-    darkgray = '30;01'
-    red = '31;01'
-    green = '32;01'
-    yellow = '33;01'
-    blue = '34;01'
-    fuchsia = '35;01'
-    turquoise = '36;01'
-    white = '37;01'
-
-    @classmethod
-    def set(cls, color, string):
-        try:
-            color = getattr(cls, color)
-        except AttributeError:
-            pass
-        return '\x1b[%sm%s\x1b[00m' % (color, string)
+from pypytools.color import Color
 
 
 def disass(obj):
