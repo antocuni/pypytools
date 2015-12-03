8b6b94171e5657c56c915a5dc7b1125f56570eee
Antonio Cuni <anto.cuni@gmail.com>
2015-12-03 12:41:46 +0000

    try to release pypytools

---
+++ setup.py
@@ -0,0 +1,19 @@
+#   Copyright 2015 Antonio Cuni anto.cuni@gmail.com
+
+from setuptools import setup
+
+desc = "A collection of useful tools to use PyPy-specific features, with CPython fallbacks"
+
+
+setup(
+    name="pypytools",
+    version="0.0.1",
+    author="Antonio Cuni",
+    author_email="anto.cuni@gmail.com",
+    url="http://bitbucket.org/antocuni/pypytools/",
+    license="MIT X11 style",
+    description=desc,
+    packages=["pypytools"],
+    long_description=desc,
+    install_requires=["py"],
+)
