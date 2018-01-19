2d4026b80f52b02460a2b494b241b20c3dde9884
Antonio Cuni <anto.cuni@gmail.com>
2018-01-19 18:10:11 +0100

    add logic to construct a step chart from pypylog events

--- pypytools/pypylog/model.py
+++ pypytools/pypylog/model.py
@@ -1,3 +1,6 @@
+import itertools
+import numpy as np
+
 class PyPyLog(object):
 
     def __init__(self):
@@ -9,3 +12,58 @@ class PyPyLog(object):
     def all_events(self):
         return self._events
 
+
+
+class Series(object):
+
+    def __init__(self, n, dtype='f'):
+        self.X = np.empty(n, dtype=dtype)
+        self.Y = np.empty(n, dtype=dtype)
+
+    @classmethod
+    def from_points(cls, points):
+        res = cls(len(points))
+        for i, (x, y) in enumerate(points):
+            res.X[i] = x
+            res.Y[i] = y
+        return res
+
+    def __len__(self):
+        assert len(self.X) == len(self.Y)
+        return len(self.X)
+
+    def __iter__(self):
+        for x, y in itertools.izip(self.X, self.Y):
+            yield x, y
+
+    def __getitem__(self, i):
+        return self.X[i], self.Y[i]
+
+    def __setitem__(self, i, p):
+        x, y = p
+        self.X[i] = x
+        self.Y[i] = y
+
+def make_step_chart(events):
+    """
+    Construct a Series which appears like a step chart when you draw it using
+    connect='pairs'
+    """
+    # for each event we draw three lines, i.e. 6 points
+    n = len(events)
+    s = Series(n*6)
+    i = 0
+    for name, start, end in events:
+        delta = end - start
+        s[i+0] = (start, 0)
+        s[i+1] = (start, delta)
+        #
+        s[i+2] = (start, delta)
+        s[i+3] = (end, delta)
+        #
+        s[i+4] = (end, delta)
+        s[i+5] = (end, 0)
+        #
+        i += 6
+    #
+    return s
---
+++ pypytools/pypylog/testing/test_model.py
@@ -0,0 +1,50 @@
+from pypytools.pypylog import model
+
+class TestSeries(object):
+
+    def test_from_points(self):
+        s = model.Series.from_points([(0, 1), (1, 2), (2, 4), (3, 8)])
+        assert len(s) == 4
+        assert list(s.X) == [0, 1, 2, 3]
+        assert list(s.Y) == [1, 2, 4, 8]
+
+    def test_iter(self):
+        plist = [(0, 1), (1, 2), (2, 4), (3, 8)]
+        s = model.Series.from_points(plist)
+        plist2 = list(s)
+        assert plist == plist2 
+
+    def test_get_set_item(self):
+        s = model.Series.from_points([(0, 0), (1, 1)])
+        assert s[0] == (0, 0)
+        assert s[1] == (1, 1)
+        s[1] = (2, 2)
+        assert s[1] == (2, 2)
+
+
+def test_make_step_chart():
+    events = [
+        ('gc',  100, 120),
+        ('jit', 130, 135)
+    ]
+    s = model.make_step_chart(events)
+    points = list(s)
+    assert points == [
+        (100, 0),
+        (100, 20),
+        
+        (100, 20),
+        (120, 20),
+
+        (120, 20),
+        (120, 0),
+
+        (130, 0),
+        (130, 5),
+
+        (130, 5),
+        (135, 5),
+
+        (135, 5),
+        (135, 0),
+    ]
