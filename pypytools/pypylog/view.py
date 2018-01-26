785e1ac572b168f355a29664856db0d52789e83b
Antonio Cuni <anto.cuni@gmail.com>
2018-01-26 17:02:32 +0100

    make it possible to toggle curve visibility by clicking on the legend

--- pypytools/pypylog/view.py
+++ pypytools/pypylog/view.py
@@ -15,11 +15,47 @@ class LogViewer(QtCore.QObject):
         # main window
         self.win = pg.GraphicsWindow(title=fname)
         self.win.installEventFilter(self) # capture key presses
+        self.scene = self.win.scene()
         #
         # main plot item, inside the window
         self.plot_item = self.win.addPlot()
         self.legend = self.plot_item.addLegend()
         self.make_charts()
+        #
+        # XXX: if we call this method, the program segfaults when exiting; not
+        # sure what is it about
+        self.make_legend_handlers()
+
+    def make_legend_handlers(self):
+        # toggle visibility of plot by clicking on the legend
+        for sample, label in self.legend.items:
+            def clicked(ev, sample=sample, label=label):
+                name = label.text
+                curve = self.get_curve(name)
+                if curve is None:
+                    print 'Cannot find curve', name
+                    return
+                self.set_curve_visibility(curve, sample, label,
+                                          not curve.isVisible())
+            #
+            sample.mouseClickEvent = clicked
+            label.mouseClickEvent = clicked
+
+    def get_curve(self, name):
+        for curve in self.plot_item.curves:
+            if curve.name() == name:
+                return curve
+        return None
+
+    def set_curve_visibility(self, curve, sample, label, visible):
+        if visible:
+            sample.setOpacity(1)
+            label.setOpacity(1)
+            curve.show()
+        else:
+            sample.setOpacity(0.5)
+            label.setOpacity(0.5)
+            curve.hide()
 
     @staticmethod
     def global_config():
