3fb945ea6acbfa7131e4d672e854938e484d6f7a
Antonio Cuni <anto.cuni@gmail.com>
2018-10-30 11:54:29 +0100

    add the logic to estimate the allocation rate

--- pypytools/gc/testing/test_uniform.py
+++ pypytools/gc/testing/test_uniform.py
@@ -1,9 +1,11 @@
+from freezegun import freeze_time
 from pypytools.gc.uniform import UniformGcStrategy
+
 
 class TestUniformGcStrategy(object):
 
-    def new(self, **kwds):
-        s = UniformGcStrategy()
+    def new(self, initial_mem=0, **kwds):
+        s = UniformGcStrategy(initial_mem)
         s.__dict__.update(**kwds)
         return s
 
@@ -17,3 +19,19 @@ class TestUniformGcStrategy(object):
         #
         s.compute_target_memory(mem=500)
         assert s.target_memory == 270 # 180*1.5, limited by GROWTH
+
+    def test_alloc_rate(self):
+        with freeze_time('2018-01-01') as freezer:
+            s = self.new(initial_mem=100)
+
+            freezer.tick(0.5)           # 0.5 second
+            s.tick(mem=150)             # delta_mem == 50
+            assert s.alloc_rate == 100  # 50/0.5 bytes/s
+            #
+            freezer.tick(2)             # 2 seconds
+            s.tick(mem=250)             # delta_mem == 100
+            assert s.alloc_rate == 75   # because of the average
+            #
+            freezer.tick(1)
+            s.tick(mem=100)             # negative delta_mem
+            assert s.alloc_rate == 37.5 # capped at 0
--- pypytools/gc/uniform.py
+++ pypytools/gc/uniform.py
@@ -2,6 +2,9 @@
 The goal of this GC strategy is to spread the GC activity as evenly as
 possible.
 """
+import time
+from collections import deque
+
 
 KB = 1024.0
 MB = KB * KB
@@ -16,14 +19,48 @@ class UniformGcStrategy(object):
     # value of target_memory: the goal is to complete a full major GC cycle
     # just before we reach target_memory.  Once we have the target_memory, we
     # can compute the actual threshold for when to *start* a GC cycle
-    
     MAJOR_COLLECT = 1.82  # same as PYPY_GC_MAJOR_COLLECT
     GROWTH = 1.4          # same as PYPY_GC_GROWTH
 
-    def __init__(self):
+    def __init__(self, initial_mem):
         self.target_memory = 10 * MB # reasonable value for the first collection
+        #
+        self.last_time = time.time()
+        self.last_mem = initial_mem
+        self.alloc_rate = None
+
+    # ======================================================================
+    # Public API
+    # ======================================================================
 
     # in all the code, the parameter "mem" indicates the memory currently used
+    def tick(self, mem):
+        """
+        Regularly called by the user program. Return True if it is time to run a
+        GC step.
+        """
+        cur_time = time.time()
+        self.update_alloc_rate(cur_time, mem)
+
+        self.last_time = cur_time
+        self.last_mem = mem
+    
+
+    # ======================================================================
+    # Private API
+    # ======================================================================
+
     def compute_target_memory(self, mem):
         self.target_memory = min(mem * self.MAJOR_COLLECT,
                                  self.target_memory * self.GROWTH)
+
+    def update_alloc_rate(self, cur_time, mem):
+        delta_t = cur_time - self.last_time
+        delta_mem = mem - self.last_mem
+        cur_alloc_rate = delta_mem / delta_t # bytes/s
+        cur_alloc_rate = max(0, cur_alloc_rate) # never use a negative alloc_rate
+        if self.alloc_rate is None:
+            self.alloc_rate = cur_alloc_rate
+        else:
+            # equivalent to an exponential moving average
+            self.alloc_rate = (self.alloc_rate + cur_alloc_rate) / 2.0
