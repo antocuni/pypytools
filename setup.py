f8e69d81926f8f86c970da988a7f6a8ad606d8df
Antonio Cuni <anto.cuni@gmail.com>
2019-04-18 17:52:45 +0200

    forgot to include the new package

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ desc = "A collection of useful tools to use PyPy-specific features, with CPython
 
 setup(
     name="pypytools",
-    version="0.6",
+    version="0.6.1",
     author="Antonio Cuni",
     author_email="anto.cuni@gmail.com",
     url="http://bitbucket.org/antocuni/pypytools/",
     license="MIT X11 style",
     description=desc,
-    packages=["pypytools", "pypytools.compat", "pypytools.gc"],
+    packages=["pypytools", "pypytools.compat", "pypytools.gc",
+              "pypytools.compat.micronumpy"],
     long_description=desc,
     install_requires=["py"],
 )
