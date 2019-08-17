c96baaf354e03151e01ededb218d1938ca2a0121
Antonio Cuni <anto.cuni@gmail.com>
2019-08-17 20:05:07 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ desc = "A collection of useful tools to use PyPy-specific features, with CPython
 
 setup(
     name="pypytools",
-    version="0.6.1",
+    version="0.6.2",
     author="Antonio Cuni",
     author_email="anto.cuni@gmail.com",
     url="http://bitbucket.org/antocuni/pypytools/",
